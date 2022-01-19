76edbb90cadf6b9124f44677fa90fb315486156a
MetaCoderX <softwareguyhuron@gmail.com>
2022-01-19 22:35:12 +1100

    Added more mobile friendly design

--- dist/public/js/top.js
+++ dist/public/js/top.js
@@ -13,7 +13,8 @@ let login_modal = document.querySelector('.login-container');
 signin_modal_btn.addEventListener('click', () => {
     console.log("Sign in btn clicked");
     login_modal.style.display = 'flex';
-    document.getElementsByTagName('html')[0].style.overflow = 'hidden';
+    document.getElementById('viewport-wrapper').style.overflowY = 'hidden';
+
 });
 
 signout_modal_btn.addEventListener('click', () => {
@@ -21,8 +22,8 @@ signout_modal_btn.addEventListener('click', () => {
     signin_modal_btn.style.display = 'block';
     signout_modal_btn.style.display = 'none';
     account_info.innerHTML = "You're not logged in yet";
-    document.getElementsByTagName('html')[0].style.overflow = 'scroll';
-    
+
+
     setTimeout(() => {
         login_modal.style.display = 'none';
         fetch('/cookie/remove')
@@ -39,40 +40,6 @@ signout_modal_btn.addEventListener('click', () => {
 })
 
 
-// window.addEventListener("scroll", function () {
-//     if ((document.documentElement.scrollTop >= 50 || document.body.scrollTop >= 50)) {
-//         // console.log("hi");
-//         if (!navContainer[0].classList.contains('nav-container-bg-color')) {
-//             navContainer[0].classList.add('nav-container-bg-color');
-//         }
-//     } else if ((document.body.scrollTop < 50 || document.documentElement.scrollTop < 50) && navContainer[0].classList.contains('nav-container-bg-color')) {
-//         navContainer[0].classList.remove('nav-container-bg-color');
-//     }
-// });
-
-
-// // Show side-sub-content-container
-// document.querySelectorAll('.side-new-content').forEach(row => {
-//         row.addEventListener('click', () => {
-//             let subContainer = document.getElementById('side-sub-content-container');
-//             let mainContainer = document.getElementById('side-content-container');
-//             subContainer.style.display = 'block';
-//             mainContainer.style.display = 'none';
-//             sideMenu.scrollTo(0, 0);
-//         });
-// });
-
-// document.getElementById('main-menu').addEventListener('click', () => {
-
-//     let subContainer = document.getElementById('side-sub-content-container');
-//     let mainContainer = document.getElementById('side-content-container');
-//     subContainer.style.display = 'none';
-//     mainContainer.style.display = 'block';
-//     subContentContainer.innerHTML = ""; // reset all pressed behaviours
-// });
-
-
-
 function dropDownSearch() {
     mobile_search_container.style.display = 'block';
     close_search.style.display = 'block';
@@ -82,4 +49,3 @@ function closeDropDownSearch() {
     mobile_search_container.style.display = 'none';
     close_search.style.display = 'none';
 }
-
